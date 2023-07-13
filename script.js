bbcf987bac3e2ca0b97adc3fef2a7d47ab04d6d2
Afan Khan <mrkhanads@gmail.com>
2023-07-13 17:12:00 +0530

    Updated the API endpoint

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 "use strict";
 
-fetch("https://api.quotable.io/random")
+fetch("https://api.quotable.io/quotes/random")
   .then((response) => response.json())
   .then((quote) => {
-    document.querySelector(".heading").textContent = quote.content;
-    document.querySelector(".author").textContent = quote.author;
+    document.querySelector(".heading").textContent = quote[0].content;
+    document.querySelector(".author").textContent = `${quote[0].author}`;
   });
